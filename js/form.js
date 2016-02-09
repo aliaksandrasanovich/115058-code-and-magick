d5549ec00142d92bd010f5a03e26cc2e8986d3b1
Asanovich Aliaksandr <asan000@mail.ru>
2016-02-09 18:19:44 +0300

    Модуль 3. Задание 1.

--- js/form.js
+++ js/form.js
@@ -14,4 +14,61 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  var labelText = document.querySelector('.review-fields-text');
+  var labelName = document.querySelector('.review-fields-name');
+  var labelContainer = document.querySelector('.review-fields');
+
+  var formElement = document.forms['review-form'];
+  var mark = formElement['review-mark'];
+  var name = formElement['review-name'];
+  var text = formElement['review-text'];
+  var submit = formElement['review-submit'];
+  validate();
+
+  for (var i = 0; i < mark.length; i++) {
+    mark[i].onclick = function(evt){
+      validate('text');
+    }
+  }
+
+  name.oninpute = function(evt){
+    validate('name');
+  }
+
+  text.onchange = function(evt){
+    validate('text');
+  }
+
+  function validate(fieldName){
+    switch (fieldName) {
+      case 'name':
+        if (name.value.trim().length === 0) {
+          labelName.style.visibility = 'visible';
+        } else {
+          labelName.style.visibility = 'hidden'
+        }
+        break;
+
+      case 'text':
+        if (mark.value < 3) {
+          if (text.value.trim().length === 0) {
+            labelText.style.visibility = 'visible';
+          } else {
+            labelText.style.visibility = 'hidden';
+          }
+        } else {
+          labelText.style.visibility = 'hidden';
+        }
+        break;
+    }
+
+    if (labelName.style.visibility === 'hidden' && labelText.style.visibility === 'hidden') {
+      labelContainer.style.visibility = 'hidden';
+      submit.removeAttribute('disabled');
+    } else {
+      labelContainer.style.visibility = 'visible';
+      submit.setAttribute('disabled', 'disabled');
+    }
+  };
 })();
